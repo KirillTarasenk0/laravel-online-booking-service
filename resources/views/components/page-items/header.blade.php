4b1b49f6bb2a4e654aabf1dc09bcd102791ee1c9
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-03-24 23:30:42 +0300

    Header layout was made

---
+++ resources/views/components/page-items/header.blade.php
@@ -0,0 +1,43 @@
+<header class="pl-14 pr-14 pt-3 pb-5">
+    <div class="flex justify-between content-center">
+        <div>
+            <img src="{{ asset('images/icons/logos/logo.svg') }}" alt="Website logo"/>
+        </div>
+        <div class="pt-4">
+            <div>
+                <a class="font-bold" href="tel:+7 (495) 677-17-79">+7 (495) 677-17-79</a>
+            </div>
+            <p class="mt-2">Ежедневно с 10:00 до 20:00</p>
+        </div>
+        <div class="flex justify-between content-center pt-4">
+            <div class="mr-4">
+                <a href="#"><img src="{{ asset('images/icons/socialnetworks/facebook-icon.svg') }}" alt="Facebook logo"/></a>
+            </div>
+            <div class="mr-4">
+                <a href="#"><img src="{{ asset('images/icons/socialnetworks/vk-icon.svg') }}" alt="VK logo"/></a>
+            </div>
+            <div>
+                <a href="#"><img src="{{ asset('images/icons/socialnetworks/instagram-icon.svg') }}" alt="Instagram logo"/></a>
+            </div>
+        </div>
+        <div class="flex justify-between content-center mt-6 border-2 pt-2.5 pl-3 pr-3 pb-2.5">
+            <div class="flex justify-between content-center">
+                <div>
+                    <img src="{{ asset('images/icons/russiaicon.svg') }}" alt="Russian flag"/>
+                </div>
+                <h4 class="ml-3 border-r-2 pr-5">RUS</h4>
+            </div>
+            <div class="ml-5">
+                <h4>Руб.</h4>
+            </div>
+        </div>
+        <div class="flex justify-between content-center pt-7">
+            <div>
+                <img src="{{ asset('images/icons/profile.svg') }}" alt="User profile">
+            </div>
+            <div class="mt-1 ml-2">
+                <a class="font-bold" href="#">Личный кабинет</a>
+            </div>
+        </div>
+    </div>
+</header>
