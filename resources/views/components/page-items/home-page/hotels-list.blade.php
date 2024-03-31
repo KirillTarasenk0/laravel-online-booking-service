1e058f6f7517c96f84ce7c13ed4064d0ad3666d6
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-03-31 22:35:52 +0300

    Some styles in hotel card connected with descritpion were fixed

--- resources/views/components/page-items/home-page/hotels-list.blade.php
+++ resources/views/components/page-items/home-page/hotels-list.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ asset('images/hotels/preview/room1.svg') }}" alt="Изображение комнаты отеля"/>
         </div>
         <h5 class="font-bold">{{ $hotel->name }}</h5>
-        <p class="text-gray-500">{{ $hotel->address }}</p>
+        <p class="text-gray-500 w-24">{{ $hotel->address }}</p>
         <div class="flex bg-blue-100 p-2 ">
             <h6 class="font-bold text-blue-600">8 990 ₽</h6>
             <p class="pl-1 text-gray-500">/ за ночь</p>
